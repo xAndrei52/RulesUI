91e02c21e997419f4dd298dc7511e664bc151b07
XSlimeX <49905629+XSlimeX@users.noreply.github.com>
2019-08-06 23:08:11 +0300

    Create Main.php

---
+++ src/RulesUI/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace RulesUI;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+	
+ public function onEnable() : void{
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);    
+    $this->getLogger()->info(C::GREEN . "Enabled!");
+		@mkdir($this->getDataFolder());
+    $this->saveDefaultConfig();
+    $this->getResource("config.yml");
+		}
+		
+	public function onJoin(PlayerJoinEvent $event) {
+		$player = $event->getPlayer();
+    $msg = $this->getConfig()->get("msg");
+		$player->sendMessage($msg);
+    $this->openMyForm($player);
+		}
+    public function openMyForm($player){ 
+        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $form = $api->createSimpleForm(function (Player $player, int $data = null){
+            $result = $data;
+            if($result === null){
+                return true;
+            }             
+            switch($result){
+                case 0:                    
+                break;                     
+            }
+            
+            
+            });
+            $title = $this->getConfig()->get("title");
+            $content = $this->getConfig()->get("content");
+            $button = $this->getConfig()->get("button");
+            $form->setTitle($title);
+            $form->setContent($content);
+            $form->addButton($button);
+            $form->sendToPlayer($player);                  
+            return $form;                                            
+				}
+	}
